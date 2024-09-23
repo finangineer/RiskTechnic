578490c8a70da353ea125bb8f8c657928ead6132
finangineer <surgunk@itu.edu.tr>
2024-09-23 17:35:23 +0300

    Initial commit

---
+++ R/Historical.Simulation.R
@@ -0,0 +1,24 @@
+#' Historical Simulation Method
+#'
+#' @param data Vector of Asset Returns
+#' @param confidence.level confidence level for the risk calculation, e.g. 95%
+#'
+#' @return VaR and ES estimates based on a risk level
+#' @export
+#'
+#' @examples Historical.Simulation(USD/TRY,confidence.level=0.95)
+Historical.Simulation <- function(data,confidence.level=0.95){
+
+  # Calculate VaR as the quantile of historical returns
+  var.forecast <- -quantile(data, probs = (1 - confidence.level))
+
+  # Calculate ES as the mean of returns worse than VaR
+  exceedances <- data[data <= -var.forecast]
+  # If there are no returns worse than VaR, set ES to NA
+  es.forecast <- ifelse(length(exceedances) > 0, -mean(exceedances), NA)
+
+  output <- cbind(var.forecast,es.forecast)
+  colnames(output) <- c("VaR","ES")
+
+  return(output)
+}
